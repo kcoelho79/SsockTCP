91bf47412fe91d6d2e35167aef973399d6f1a335
Kleber iMac <kleber.amg79@gmail.com>
2020-12-11 18:02:38 -0200

    customizacoes

--- zen_utils.py
+++ zen_utils.py
@@ -3,16 +3,10 @@
 # https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter07/zen_utils.py
 # Constants and routines for supporting a certain network conversation.
 
-import argparse, socket, time
+#TODO transform to Class, __init__  create socket
 
-aphorisms = {b'Beautiful is better than?': b'Ugly.',
-             b'Explicit is better than?': b'Implicit.',
-             b'Simple is better than?': b'Complex.'}
+import argparse, socket, sys
 
-def get_answer(aphorism):
-    """Return the string response to a particular Zen-of-Python aphorism."""
-    time.sleep(0.0)  # increase to simulate an expensive operation
-    return aphorisms.get(aphorism, b'Error: unknown aphorism.')
 
 def parse_command_line(description):
     """Parse command line and return a socket address."""
@@ -24,11 +18,21 @@ def parse_command_line(description):
     address = (args.host, args.p)
     return address
 
+def create_socket():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    return sock
+
+def create_cli_connection(address):
+    """ create socket and connect server at address """
+    sock = create_socket()
+    sock.connect(address)
+    print('connected at {}'.format(sock.getpeername()))
+    return sock
+
 def create_srv_socket(address):
     """Build and return a listening server socket."""
-    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listener = create_socket()
     listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    #set_keepalive_osx(listener)
     listener.bind(address)
     listener.listen(64)
     print('Listening at {}'.format(address))
@@ -55,40 +59,24 @@ def handle_conversation(sock, address):
 
 def handle_request(sock):
     """Receive a single client request on `sock` and send the answer."""
-    msg = recv_until(sock)
-    sock.sendall(msg)
+    message = recv_until(sock)
+    sock.sendall(message)
 
 def recv_until(sock):
     """Receive bytes over socket `sock` until we receive the `suffix`."""
-    message = sock.recv(4096)
+    message = sock.recv(2048)
+
     if not message:
         raise EOFError('socket closed')
 
-    while not message.endswith(b'\n'):
-        data = sock.recv(4096)
-        if not data:
-            raise IOError('received {!r} then socket closed'.format(message))
-        message += data
+# TODO: how to do to keep connetio until get warning for close
+
+    # while not message.endswith(b'\n'):
+    #     data = sock.recv(4096)
+    #     if not data:
+    #         raise IOError('received {!r} then socket closed'.format(message))
+    #     message += data
     return message
 
-def set_keepalive_linux(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
-    """Set TCP keepalive on an open socket.
-
-    It activates after 1 second (after_idle_sec) of idleness,
-    then sends a keepalive ping once every 3 seconds (interval_sec),
-    and closes the connection after 5 failed ping (max_fails), or 15 seconds
-    """
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
-
-def set_keepalive_osx(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
-    """Set TCP keepalive on an open socket.
-
-    sends a keepalive ping once every 3 seconds (interval_sec)
-    """
-    # scraped from /usr/include, not exported by python's socket module
-    TCP_KEEPALIVE = 0x10
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-    sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPALIVE, interval_sec)
+def close_connection(sock):
+    sock.close()
